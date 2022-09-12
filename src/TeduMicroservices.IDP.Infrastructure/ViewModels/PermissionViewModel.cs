8ffa349c8116fa3763da76265ddbd32329857fb5
Kiet Pham <kietpham.dev@gmail.com>
2022-09-13 00:01:32 +1000

    PermissionViewModel

--- src/TeduMicroservices.IDP.Infrastructure/ViewModels/PermissionViewModel.cs
+++ src/TeduMicroservices.IDP.Infrastructure/ViewModels/PermissionViewModel.cs
@@ -1,6 +1,8 @@
+using TeduMicroservices.IDP.Infrastructure.Domains;
+
 namespace TeduMicroservices.IDP.Infrastructure.ViewModels;
 
-public class PermissionViewModel
+public class PermissionViewModel : EntityBase<long>
 {
     public string RoleId { get; set; }
 
