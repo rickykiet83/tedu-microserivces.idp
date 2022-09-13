c0a329ef04e0c1049984db0ea9022cdc75ba1500
Kiet Pham <kietpham.dev@gmail.com>
2022-09-13 11:15:06 +1000

    ADD Create Permission store

---
+++ DatabaseScripts/Store Procedures/Create_Permission.sql
@@ -0,0 +1,50 @@
+USE [TeduIdentity]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF EXISTS ( SELECT *
+FROM sysobjects
+WHERE  id = object_id(N'[Create_Permission]')
+	and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+	DROP PROCEDURE [Create_Permission]
+END
+
+GO
+
+CREATE PROCEDURE [Create_Permission]
+	@roleId VARCHAR(50) NULL,
+	@function varchar(50) NULL,
+	@command varchar(50) NULL,
+	@newID bigint OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	BEGIN TRAN
+	BEGIN TRY
+
+	IF NOT EXISTS (SELECT *
+	FROM [Identity].Permissions
+	WHERE [RoleId] = @roleId AND
+		[Function] = @function AND
+		[Command] = @command)
+   	BEGIN
+		INSERT INTO [Identity].Permissions
+			([RoleId], [Function], [Command])
+		VALUES
+			(@roleId, @function, @command)
+		SET @newID = SCOPE_IDENTITY();
+	END
+	
+	COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		  DECLARE @ErrorMessage VARCHAR(2000)
+		  SELECT @ErrorMessage = 'ERROR: ' + ERROR_MESSAGE()
+		  RAISERROR(@ErrorMessage, 16, 1)
+	END CATCH
+END
