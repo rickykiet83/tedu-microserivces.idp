c4b52ef13fb8a130ee3206ffb31811f62b8a4fa7
Kiet Pham <kietpham.dev@gmail.com>
2022-09-24 18:32:15 +1000

    update config

--- src/TeduMicroservices.IDP/Config.cs
+++ src/TeduMicroservices.IDP/Config.cs
@@ -54,18 +54,21 @@ public static class Config
                     "http://localhost:5020/swagger/oauth2-redirect.html",
                     "http://localhost:5001/swagger/oauth2-redirect.html",
                     "http://localhost:5002/swagger/oauth2-redirect.html",
+                    "http://localhost:6002/swagger/oauth2-redirect.html",
                 },
                 PostLogoutRedirectUris = new List<string>()
                 {
                     "http://localhost:5020/swagger/oauth2-redirect.html",
                     "http://localhost:5001/swagger/oauth2-redirect.html",
                     "http://localhost:5002/swagger/oauth2-redirect.html",
+                    "http://localhost:6002/swagger/oauth2-redirect.html",
                 },
                 AllowedCorsOrigins = new List<string>()
                 {
                     "http://localhost:5020",
                     "http://localhost:5001",
                     "http://localhost:5002",
+                    "http://localhost:6002",
                 },
                 AllowedScopes =
                 {
