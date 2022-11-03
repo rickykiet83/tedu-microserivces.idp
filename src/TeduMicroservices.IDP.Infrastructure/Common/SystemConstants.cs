5f633153088e1f13b39a7a5f0391b91c5145842a
Kiet Pham <kietpham.dev@gmail.com>
2022-11-03 20:10:09 +1100

    fixed wrong Permissions typing

--- src/TeduMicroservices.IDP.Infrastructure/Common/SystemConstants.cs
+++ src/TeduMicroservices.IDP.Infrastructure/Common/SystemConstants.cs
@@ -7,7 +7,7 @@ public static class SystemConstants
     public static class Claims
     {
         public const string Roles = "roles";
-        public const string Permissions = "permisssions";
+        public const string Permissions = "permissions";
         public const string UserId = "id";
         public const string UserName = "userName";
         public const string FirstName = "firstName";
